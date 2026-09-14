f48b737ec1609b0d1586efdbd45bc283b80e2965
Vishal Mahto <vishal.kumar.connect0@gmail.com>
2026-09-14 15:26:33 +0530

    array started

---
+++ java_training/array1.java
@@ -0,0 +1,8 @@
+import java.util.Arrays;
+class Array1{
+    public static void main(String args[]){
+        int[] x = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(x));
+        System.out.println(x.length);
+    }
+}
